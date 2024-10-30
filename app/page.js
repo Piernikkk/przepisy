8331daa1970690c25231c416814a1890908440f7
Michał Ziernik <ziernikmichal@gmail.com>
2024-10-30 20:06:13 +0100

    search

--- app/page.js
+++ app/page.js
@@ -1,17 +1,22 @@
 'use client';
+import Modal from "@/lib/components/modal/Modal";
 import Header from "@/lib/items/Header";
 import Item from "@/lib/items/Item";
 import ItemContainer from "@/lib/items/ItemContainer";
 import { RecipesContext } from "@/lib/providers/RecipiesProvider";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState('');
 
   const { recipes, dispatch } = useContext(RecipesContext);
+  const [filters, setFilters] = useState([{ label: 'Show only favourites', name: 'fav', value: false, visible: true }, { name: 'search', value: '', visible: false }]);
+
   return (
     <div>
-      <Header />
-      <ItemContainer recipes={recipes} dispatch={dispatch} />
+      <Modal />
+      <Header filters={filters} setFilters={setFilters} searchValue={searchValue} setSearchValue={setSearchValue} />
+      <ItemContainer recipes={recipes} dispatch={dispatch} filters={filters} searchValue={searchValue} />
 
     </div>
   );
