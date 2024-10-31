a624764b7eb32f9a941591b152c6ef87dc0b3925
Michał Ziernik <ziernikmichal@gmail.com>
2024-10-31 23:54:52 +0100

    modal

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 'use client';
+import AddRecipeModal from "@/lib/components/modal/AddRecipeModal";
 import Modal from "@/lib/components/modal/Modal";
+import useDisclosure from "@/lib/components/useDisclosure";
 import Header from "@/lib/items/Header";
 import Item from "@/lib/items/Item";
 import ItemContainer from "@/lib/items/ItemContainer";
@@ -8,14 +10,15 @@ import { useContext, useState } from "react";
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState('');
+  const [AddRecipeOpened, AddRecipe] = useDisclosure();
 
   const { recipes, dispatch } = useContext(RecipesContext);
   const [filters, setFilters] = useState([{ label: 'Show only favourites', name: 'fav', value: false, visible: true }, { name: 'search', value: '', visible: false }]);
 
   return (
     <div>
-      <Modal />
-      <Header filters={filters} setFilters={setFilters} searchValue={searchValue} setSearchValue={setSearchValue} />
+      <AddRecipeModal opened={AddRecipeOpened} onClose={AddRecipe.close} />
+      <Header filters={filters} setFilters={setFilters} searchValue={searchValue} setSearchValue={setSearchValue} onAddClick={AddRecipe.open} />
       <ItemContainer recipes={recipes} dispatch={dispatch} filters={filters} searchValue={searchValue} />
 
     </div>
